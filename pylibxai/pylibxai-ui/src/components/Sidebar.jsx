46b46d8305d8d142820419512dfe312348234ea2
Maciej Falkowski <maciej.falkowski9@gmail.com>
2025-05-23 17:30:58 +0200

    Add missing sidebar content

--- pylibxai/pylibxai-ui/src/components/Sidebar.jsx
+++ pylibxai/pylibxai-ui/src/components/Sidebar.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle, faChartBar } from '@fortawesome/free-solid-svg-icons';
+import { 
+  faInfoCircle, 
+  faChartBar, 
+  faLightbulb, 
+  faBrain,
+  faCircleInfo 
+} from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
 
 function Sidebar() {
@@ -15,6 +21,18 @@ function Sidebar() {
         <Nav.Link href="#shap" className="d-flex align-items-center">
           <FontAwesomeIcon icon={faChartBar} className="me-2" />
           SHAP
+        </Nav.Link>
+        <Nav.Link href="#lime" className="d-flex align-items-center">
+          <FontAwesomeIcon icon={faLightbulb} className="me-2" />
+          LIME
+        </Nav.Link>
+        <Nav.Link href="#lrp" className="d-flex align-items-center">
+          <FontAwesomeIcon icon={faBrain} className="me-2" />
+          LRP
+        </Nav.Link>
+        <Nav.Link href="#inference" className="d-flex align-items-center">
+          <FontAwesomeIcon icon={faCircleInfo} className="me-2" />
+          Inference Information
         </Nav.Link>
       </Nav>
     </div>
